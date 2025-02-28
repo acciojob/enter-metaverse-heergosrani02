e2675a7579eb475079db7efbb4623a1b28ce2c78
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-28 23:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let enterMeta = document.getElementById("status");
+let btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click", () => {
+	// let enteredMeta = document.createElement("h1");
+	enterMeta.innerHTML = '<h1>Entered Metaverse</h1>';
+	// enterMeta.replaceWith(enteredMeta);
+})
